cbe4caab2cc0340ed17937f0598f1eb5c4d322c7
Ankit Hans <ankithans1947@gmail.com>
2020-11-04 15:13:11 +0530

    feat: add server and client

---
+++ server/database/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const connectDB = require("./database/db");
+const bodyParser = require("body-parser");
+const app = express();
+require("dotenv").config();
+
+// db connection
+connectDB();
+
+// middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "GG",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server started on http://localhost:${PORT}`)
+);
