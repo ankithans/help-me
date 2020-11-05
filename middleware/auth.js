c2347082912624f4ae52b78ed73ebf384b421fa3
Ankit Hans <ankithans1947@gmail.com>
2020-11-05 12:58:44 +0530

    feat: auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  // Get the token form header
+  const token = req.header("x-auth-token");
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
