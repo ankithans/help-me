6801141af1d6eba6c6482ec62e15bd05a6129b48
Ankit Hans <ankithans1947@gmail.com>
2020-11-07 10:53:20 +0530

    feat: add local flutter notifications

--- client/helpMe/lib/main.dart
+++ client/helpMe/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:helpMe/constants.dart';
 import 'package:helpMe/pages/auth/login/ui.dart';
@@ -16,14 +17,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-
   FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
   FlutterSecureStorage secureStorage = FlutterSecureStorage();
 
+  final flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
 
-  void fcmListener(){
+  void fcmListener() {
     _firebaseMessaging.getToken().then((value) async {
       await secureStorage.write(key: 'fcmToken', value: value);
       print(value);
@@ -32,6 +32,11 @@ class _MyAppState extends State<MyApp> {
     _firebaseMessaging.configure(
       onMessage: (message) async {
         print('onMessage: $message');
+        _showNotification(
+            1234,
+            "GET title FROM message OBJECT",
+            "GET description FROM message OBJECT",
+            "GET PAYLOAD FROM message OBJECT");
       },
       onLaunch: (message) async {
         print('onLaunch: $message');
@@ -40,12 +45,62 @@ class _MyAppState extends State<MyApp> {
         print('onResume: $message');
       },
     );
+  }
 
+  Future<dynamic> onSelectNotification(String payload) async {
+    /*Do whatever you want to do on notification click. In this case, I'll show an alert dialog*/
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text(payload),
+        content: Text("Payload: $payload"),
+      ),
+    );
+  }
+
+  Future<void> _showNotification(
+    int notificationId,
+    String notificationTitle,
+    String notificationContent,
+    String payload, {
+    String channelId = '1234',
+    String channelTitle = 'Android Channel',
+    String channelDescription = 'Default Android Channel for notifications',
+    Priority notificationPriority = Priority.high,
+    Importance notificationImportance = Importance.max,
+  }) async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+      channelId,
+      channelTitle,
+      channelDescription,
+      playSound: false,
+      importance: notificationImportance,
+      priority: notificationPriority,
+    );
+    var iOSPlatformChannelSpecifics =
+        new IOSNotificationDetails(presentSound: false);
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      notificationId,
+      notificationTitle,
+      notificationContent,
+      platformChannelSpecifics,
+      payload: payload,
+    );
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
     fcmListener();
   }
 
@@ -54,21 +109,15 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'Nunito',
-        textTheme: TextTheme(
-          headline1: TextStyle(
-            fontFamily: 'Nunito',
-            color: primaryColor,
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold
-          ),
-          bodyText1: TextStyle(
-            fontFamily: 'Nunito',
-            color: fontColor,
-            fontSize: 15.0
-          )
-        )
-      ),
+          fontFamily: 'Nunito',
+          textTheme: TextTheme(
+              headline1: TextStyle(
+                  fontFamily: 'Nunito',
+                  color: primaryColor,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+              bodyText1: TextStyle(
+                  fontFamily: 'Nunito', color: fontColor, fontSize: 15.0))),
       home: LoginPage(),
     );
   }
